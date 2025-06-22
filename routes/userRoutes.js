2f501d867a7e4267fcfbdf35d67d35ed38c8cc1b
Diego Azevedo <diego.azevedo@ritmodoesporte.com.br>
2025-06-22 15:13:23 -0300

    fix(rateLimiter): adjust liter to router update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const checkToken = require('../middlewares/checkToken');
+const { loginUpdateLimiter } = require('../middlewares/rateLimiters');
 const {
   publicRoute,
   getUserById,
@@ -14,8 +15,8 @@ router.get('/', publicRoute);
 
 // Rotas privadas
 router.get('/:id', checkToken, getUserById);
-router.put('/:id', checkToken, updateUser);
-router.put('/:id/password', checkToken, updatePassword);
+router.put('/:id', loginUpdateLimiter, checkToken, updateUser);
+router.put('/:id/password', loginUpdateLimiter, checkToken, updatePassword);
 router.delete('/:id', checkToken, softDeleteUser);
 
 module.exports = router;
